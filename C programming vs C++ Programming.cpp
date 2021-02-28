261bb68cc64b498f6906623db90fdf38e04b477c
sumedha lakmal bandaranayaka <74480291+sumedhaLakmal@users.noreply.github.com>
2021-03-01 01:40:08 +0530

    C++ Basic Source Code

---
+++ C programming vs C++ Programming.cpp
@@ -0,0 +1,24 @@
+//C Programming vs C++ Programming//
+
+//C programming//
+
+//header file
+#include<stdio.h>
+
+//main function
+int main(){
+	printf("Hello World");
+	
+	return 0;
+}//end main function
+
+//header file
+#include<iostream>
+
+//main function
+int main(){
+	std::cout<<"Hello World!";
+	std::cout<<std::endl;
+	
+	return 0;
+}//end main function
---
+++ First C++ Programme.cpp
@@ -0,0 +1,20 @@
+//First C++ Programme//
+
+//header file
+#include<iostream>
+
+//main function
+int main(){
+	std::cout<<"Hello World !";//Display Massage
+	std::cout<<std::endl;//new line
+	
+	return 0;
+	
+}//end main function
+
+//Additional Details
+
+//C programming comment
+
+// - Singal Line Comment
+/*Delimited comments ,that comment use with more than one line*/ 
